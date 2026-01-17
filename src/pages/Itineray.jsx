939eb8cdecb7c53666a416d0840019f71476469e
AvaniDange <dangeavani@gmail.com>
2026-01-17 13:28:28 +0530

    Updated model

--- src/pages/Itineray.jsx
+++ src/pages/Itineray.jsx
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Initialize Google Generative AI with API key
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-3-flash-preview" });
 
 const Itinerary = () => {
   const [place, setPlace] = useState('');
--- src/pages/Travel.jsx
+++ src/pages/Travel.jsx
@@ -22,7 +22,7 @@ function Travel() {
   const [loading, setLoading] = useState(false);
 
   const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-3-flash-preview" });
 
   useEffect(() => {
     setTimeout(() => {
